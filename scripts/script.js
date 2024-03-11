f6eb8d36a69f4975630accb15d99dbb4cad68bbc
Garrett Lewis <115313310+lewisjga@users.noreply.github.com>
2024-03-11 15:50:27 -0500

    Add drawing functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,38 @@
 const gridFrame = document.createElement('div');
 gridFrame.id = 'grid-frame';
+let userInput = document.getElementById('user-input');
+let submitButton = document.getElementById('submit-button');
 
 let gridDimensions = 16;
 
-for (let i = 0; i < (gridDimensions); i++) {
-    const gridRow = document.createElement('div');
-    gridRow.className = 'etch-row';
-    for (let j = 0; j< (gridDimensions); j++) {
-        const gridPix = document.createElement('div');
-        gridPix.className = 'etch-pixel';
-        gridRow.append(gridPix);
+
+
+
+function drawGrid(dimensions){
+    //Clean up any preexisting grids
+    while(gridFrame.lastChild){
+        gridFrame.removeChild(gridFrame.lastChild);
     }
-    gridFrame.appendChild(gridRow);
+
+    for (let i = 0; i < (dimensions); i++) {
+        const gridRow = document.createElement('div');
+        gridRow.className = 'etch-row';
+        for (let j = 0; j< (dimensions); j++) {
+            const gridPix = document.createElement('div');
+            gridPix.className = 'etch-pixel';
+            gridPix.addEventListener('mouseenter',
+            () => {
+                gridPix.style.backgroundColor = 'green';
+            });
+            gridPix.addEventListener('mouseleave',
+            () => {
+                gridPix.style.backgroundColor = 'black';
+            });
+            gridRow.append(gridPix);
+        }
+        gridFrame.appendChild(gridRow);
+    }
+    document.body.appendChild(gridFrame);
 }
 
-document.body.appendChild(gridFrame);
+drawGrid(gridDimensions);
